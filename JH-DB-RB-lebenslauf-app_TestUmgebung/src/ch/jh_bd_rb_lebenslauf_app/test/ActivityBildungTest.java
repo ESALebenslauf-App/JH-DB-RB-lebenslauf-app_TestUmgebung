416f0cf188f91c17ea50f5382a7310094643344b
jherzig <info@jherzig.ch>
2014-05-08 12:20:44 +0200

    Initial Commit ESALebenslauf-App_TestUmgebung

---
+++ JH-DB-RB-lebenslauf-app_TestUmgebung/src/ch/jh_bd_rb_lebenslauf_app/test/ActivityBildungTest.java
@@ -0,0 +1,49 @@
+package ch.jh_bd_rb_lebenslauf_app.test;
+
+import com.robotium.solo.Solo;
+import ch.jh_bd_rb_lebenslauf_app.R;
+import ch.jh_bd_rb_lebenslauf_app.gui.BildungActivity;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+
+public class ActivityBildungTest extends
+		ActivityInstrumentationTestCase2<BildungActivity> {
+
+	private Solo mSimulator;
+	private EditText edt_bildung_schule;
+	private EditText edt_bildung_adresse;
+
+	public ActivityBildungTest() {
+		super("ch.jh_bd_rb_lebenslauf_app", BildungActivity.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		mSimulator = new Solo(getInstrumentation(), getActivity());
+		edt_bildung_schule = (EditText) getActivity().findViewById(
+				R.id.edt_bildung_schule);
+		edt_bildung_adresse = (EditText) getActivity().findViewById(
+				R.id.edt_bildung_adresse);
+	}
+
+	public void testBildung() {
+		mSimulator.assertCurrentActivity(
+				"Activity BildungActivity.class erwartet",
+				BildungActivity.class);
+		assertTrue("Button // Berufserfahrung/ ist nicht vorhanden",
+				mSimulator.searchButton("Berufserfahrung"));
+		assertTrue("Button // Skills/ ist nicht vorhanden",
+				mSimulator.searchButton("Skills"));
+		assertTrue("Button // Bildung hinzufügen/ ist nicht vorhanden",
+				mSimulator.searchButton("Bildung hinzufügen"));
+		
+		mSimulator.enterText(edt_bildung_adresse, "TEST Adresse Text");
+		mSimulator.enterText(edt_bildung_schule, "TEST Schule Text");
+		mSimulator.clickOnButton("Bildung hinzufügen");
+		mSimulator.enterText(edt_bildung_adresse, "TEST Adresse2 Text");
+		mSimulator.enterText(edt_bildung_schule, "TEST Schule2 Text");
+		mSimulator.clickOnButton("Bildung hinzufügen");
+		mSimulator.clickOnButton("Skills");
+	}
+
+}
---
+++ JH-DB-RB-lebenslauf-app_TestUmgebung/src/ch/jh_bd_rb_lebenslauf_app/test/ActivitysTest.java
@@ -0,0 +1,120 @@
+package ch.jh_bd_rb_lebenslauf_app.test;
+
+import com.robotium.solo.Solo;
+
+import ch.jh_bd_rb_lebenslauf_app.daten.Bildung;
+import ch.jh_bd_rb_lebenslauf_app.gui.Berufserfahrung;
+import ch.jh_bd_rb_lebenslauf_app.gui.Bild;
+import ch.jh_bd_rb_lebenslauf_app.gui.BildungActivity;
+import ch.jh_bd_rb_lebenslauf_app.gui.Finish;
+import ch.jh_bd_rb_lebenslauf_app.gui.Skills;
+import ch.jh_bd_rb_lebenslauf_app.gui.StartActivity;
+import ch.jh_bd_rb_lebenslauf_app.gui.Zusammenfassung;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class ActivitysTest extends
+		ActivityInstrumentationTestCase2<StartActivity> {
+
+	private Solo mSimulator;
+
+	public ActivitysTest() {
+		super("ch.jh_bd_rb_lebenslauf_app", StartActivity.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		mSimulator = new Solo(getInstrumentation(), getActivity());
+	}
+
+	public void test_Activitys() throws Exception {
+		assertTrue("Button // CV erfassen/ ist nicht vorhanden",
+				mSimulator.searchButton("CV erfassen"));
+		assertTrue("Text nicht richtig",
+				mSimulator.searchText("CV schnell und einfach erfassen"));
+
+		mSimulator.clickOnButton("CV erfassen");
+		mSimulator.assertCurrentActivity("Activity Bild.class erwartet",
+				Bild.class);
+		assertTrue("Button // Berufserfahrung/ ist nicht vorhanden",
+				mSimulator.searchButton("Berufserfahrung"));
+		
+		mSimulator.clickOnButton("Berufserfahrung");
+		mSimulator.assertCurrentActivity(
+				"Activity Berufserfahrung.class erwartet",
+				Berufserfahrung.class);
+		assertTrue("Button // Bild/ ist nicht vorhanden",
+				mSimulator.searchButton("Bild"));
+		assertTrue("Button // Bildung/ ist nicht vorhanden",
+				mSimulator.searchButton("Bildung"));
+
+		mSimulator.clickOnButton("Bildung");
+		mSimulator.assertCurrentActivity("Activity BildungActivity.class erwartet",
+				BildungActivity.class);
+		assertTrue("Button // Berufserfahrung/ ist nicht vorhanden",
+				mSimulator.searchButton("Berufserfahrung"));
+		assertTrue("Button // Skills/ ist nicht vorhanden",
+				mSimulator.searchButton("Skills"));
+		
+		mSimulator.clickOnButton("Skills");
+		mSimulator.assertCurrentActivity("Activity Skills.class erwartet",
+				Skills.class);
+		assertTrue("Button // Bildung/ ist nicht vorhanden",
+				mSimulator.searchButton("Bildung"));
+		assertTrue("Button // Zusammenfassung/ ist nicht vorhanden",
+				mSimulator.searchButton("Zusammenfassung"));
+		
+		mSimulator.clickOnButton("Zusammenfassung");
+		mSimulator.assertCurrentActivity("Activity Zusammenfassung.class erwartet",
+				Zusammenfassung.class);
+		assertTrue("Button // Skills/ ist nicht vorhanden",
+				mSimulator.searchButton("Skills"));
+		assertTrue("Button // Finish/ ist nicht vorhanden",
+				mSimulator.searchButton("Finish"));
+		
+		mSimulator.clickOnButton("Finish");
+		mSimulator.assertCurrentActivity("Activity Finish.class erwartet",
+				Finish.class);
+		assertTrue("Button // Zusammenfassung/ ist nicht vorhanden",
+				mSimulator.searchButton("Zusammenfassung"));
+		
+		//Zurück Navigieren
+		mSimulator.clickOnButton("Zusammenfassung");
+		mSimulator.assertCurrentActivity("Activity Zusammenfassung.class erwartet",
+				Zusammenfassung.class);
+		assertTrue("Button // Skills/ ist nicht vorhanden",
+				mSimulator.searchButton("Skills"));
+		assertTrue("Button // Finish/ ist nicht vorhanden",
+				mSimulator.searchButton("Finish"));
+		
+		mSimulator.clickOnButton("Skills");
+		mSimulator.assertCurrentActivity("Activity Skills.class erwartet",
+				Skills.class);
+		assertTrue("Button // Bildung/ ist nicht vorhanden",
+				mSimulator.searchButton("Bildung"));
+		assertTrue("Button // Zusammenfassung/ ist nicht vorhanden",
+				mSimulator.searchButton("Zusammenfassung"));
+		
+		mSimulator.clickOnButton("Bildung");
+		mSimulator.assertCurrentActivity("Activity BildungActivity.class erwartet",
+				BildungActivity.class);
+		assertTrue("Button // Berufserfahrung/ ist nicht vorhanden",
+				mSimulator.searchButton("Berufserfahrung"));
+		assertTrue("Button // Skills/ ist nicht vorhanden",
+				mSimulator.searchButton("Skills"));
+		
+		mSimulator.clickOnButton("Berufserfahrung");
+		mSimulator.assertCurrentActivity(
+				"Activity Berufserfahrung.class erwartet",
+				Berufserfahrung.class);
+		assertTrue("Button // Bild/ ist nicht vorhanden",
+				mSimulator.searchButton("Bild"));
+		assertTrue("Button // Bildung/ ist nicht vorhanden",
+				mSimulator.searchButton("Bildung"));
+		
+		mSimulator.clickOnButton("Bild");
+		mSimulator.assertCurrentActivity("Activity Bild.class erwartet",
+				Bild.class);
+		assertTrue("Button // Berufserfahrung/ ist nicht vorhanden",
+				mSimulator.searchButton("Berufserfahrung"));
+	}
+}

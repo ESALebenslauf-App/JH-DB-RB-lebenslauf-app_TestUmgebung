22a6629637bd28913d86c8e716cb19a2260228a7
jherzig <info@jherzig.ch>
2014-06-11 23:53:12 +0200

    Test für Bildung überarbeitet und erweiter.

--- JH-DB-RB-lebenslauf-app_TestUmgebung/src/ch/jh_bd_rb_lebenslauf_app/test/ActivityBildungDBTest.java
+++ JH-DB-RB-lebenslauf-app_TestUmgebung/src/ch/jh_bd_rb_lebenslauf_app/test/ActivityBildungDBTest.java
@@ -1,47 +1,62 @@
 package ch.jh_bd_rb_lebenslauf_app.test;
 
-import java.text.SimpleDateFormat;
-
-import com.robotium.solo.Solo;
-
-import ch.jh_bd_rb_lebenslauf_app.R;
+import ch.jh_bd_rb_lebenslauf_app.daten.Bildung;
 import ch.jh_bd_rb_lebenslauf_app.daten.BildungDB;
 import ch.jh_bd_rb_lebenslauf_app.gui.BildungActivity;
-import android.content.Context;
 import android.database.Cursor;
 import android.test.ActivityInstrumentationTestCase2;
-import android.widget.EditText;
 
 public class ActivityBildungDBTest extends
 		ActivityInstrumentationTestCase2<BildungActivity> {
 
-	private Solo mSimulator;
-	private EditText edt_bildung_schule;
-	private EditText edt_bildung_adresse;
-	long id = 0;
+
 
 	@SuppressWarnings("deprecation")
 	public ActivityBildungDBTest() {
 		super("ch.jh_bd_rb_lebenslauf_app", BildungActivity.class);
 	}
 
+
+
 	@Override
 	protected void setUp() throws Exception {
-		mSimulator = new Solo(getInstrumentation(), getActivity());
-		edt_bildung_schule = (EditText) getActivity().findViewById(
-				R.id.edt_bildung_schule);
-		edt_bildung_adresse = (EditText) getActivity().findViewById(
-				R.id.edt_bildung_adresse);
+
 	}
 	
-	public void test_1DB_BildungInsert(){
+	public void test_1DB_BildungInsertLoudID(){
+		Bildung bildung = getBildung();
 		BildungDB db = new BildungDB(getActivity());
 		db.open();
-		id = db.insertBildung("anrede", "bildungsart", "schulname", 4624, "ort", "von", "bis");
-		assertTrue(id > 0);
+		bildung = db.insertBildung(bildung);
+		assertTrue(bildung.getId() > 0);
+		
+		Bildung returnBildung = new Bildung(bildung.getId());
+		returnBildung = db.getBildung(returnBildung);
+		if (returnBildung != null) {
+			assertTrue(bildung.getNameschule().equals(returnBildung.getNameschule()));
+		}
+		else {
+			assertTrue(false);
+		}
+		
+		db.deleteBildung(returnBildung);
+		Bildung bildungDelet = new Bildung(returnBildung.getId());
+		bildungDelet = db.getBildung(bildungDelet);
+		if (bildungDelet != null) {
+			assertTrue(true);
+		}
+		
 		db.close();
 	}
 	
+	private Bildung getBildung() {
+		Bildung bildung = new Bildung("bildungsart", "schulname", "4624", "ort", "von", "bis");
+		return bildung;
+	}
+
+
+
+	//TODO Test überarbeiten und Liste von Bildungs Objekten zurücknhemen
 	public void test_2DB_BildungAllCursor(){
 		BildungDB db = new BildungDB(getActivity());
 		db.open();
@@ -51,20 +66,6 @@ public class ActivityBildungDBTest extends
 
 		db.close();
 	}
-	
-	public void test_3DB_BildungLoudID(){
-		int test = 0;
-		BildungDB db = new BildungDB(getActivity());
-		db.open();
-		Cursor cursor = db.getBildung("2");
-		if (cursor != null) {
-			test = cursor.getCount();
-		}
-		assertTrue(test == 1);
-		db.close();
-	}
-
-	
 	
 
 }
